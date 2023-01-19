1a44311789d80582158d4a0a3e35a84dfd735e91
saurabh bakolia <skbakolia2002@gmail.com>
2023-01-19 18:54:58 +0530

    removed unused dependency

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const { EventEmitter } = require("events");
 const port = 3000;
 
 var json_data = require('./teams.json');
@@ -12,23 +9,23 @@ var json_data = require('./teams.json');
 function sortTeam(team) {
     json_data.sort((a, b) => {
         if (a.Pts === b.Pts) {
-            return a.NRR - b.NRR;
+            return b.NRR - a.NRR;
         } else {
-            return a.Pts - b.Pts;
+            return b.Pts - a.Pts;
         }
     });
 }
-const dom = new JSDOM(`index.pug`);
 
-console.log(dom.window.document.getElementsByClassName("btn"));
-
+sortTeam();
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+const firstEmitter = new EventEmitter();
 app.get('/', (req, res) => {
+    firstEmitter.emit("My first event");
     res.render('index.pug', { json_data });
 
 })
---
+++ event.js
@@ -0,0 +1,2 @@
+const button = document.getElementsByClassName('btn');
+console.log(button);
